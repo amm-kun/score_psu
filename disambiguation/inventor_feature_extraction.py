fdeed77bfc072ccbe3c8bf7070acdfab7af300e4
Arjun Menon <amm8987@psu.edu>
2020-03-02 03:42:34 -0500

    All exceptions handled except class based query ones.

--- disambiguation/inventor_feature_extraction.py
+++ disambiguation/inventor_feature_extraction.py
@@ -10,6 +10,7 @@ Usage: inventor_feature_extraction.py -f <input>
 
 from utilities import mysql_breckenridge, check_file, patent_sections
 from mysql_conn import connect_db
+from mysql.connector.errors import InternalError
 import argparse
 import csv
 
@@ -18,7 +19,7 @@ con = connect_db(**mysql_breckenridge)
 cursor = con.cursor()
 
 writer = csv.writer(open("inventors.csv", 'w'))
-writer.writerow(['patent_id', 'inventor_id', 'name_first', 'name_last', 'section', 'subsection',
+writer.writerow(['patent_id', 'title', 'inventor_id', 'name_first', 'name_last', 'section', 'subsection',
                  'group', 'sub_group',  'city', 'state', 'long|lat', 'organization'])
 # Co-authors for patent?
 
@@ -35,24 +36,18 @@ with open(file.file) as inventor_list:
 for inventor in inventor_ids:
     patent_id, sequence = inventor
 
+    patent_query = "SELECT title from patent where id = '{0}'".format(patent_id)
+    cursor.execute(patent_query)
+    title = cursor.fetchone()
+
     inventor_query = ("SELECT inventor_id, rawlocation_id, name_first, name_last FROM rawinventor WHERE"
-                      " patent_id = {0} and sequence = {1}".format(patent_id, sequence))
-    try:
-        cursor.execute(inventor_query)
-    except TypeError as e:
-        print(e)
-        print("Couldn't process ", patent_id)
-        continue
+                      " patent_id = '{0}' and sequence = {1}".format(patent_id, sequence))
+    cursor.execute(inventor_query)
     (inventor_id, rawloc_id, name_first, name_last) = cursor.fetchone()
 
     loc_query = "SELECT location_id, city, state from rawlocation where id = '{0}'".format(rawloc_id)
     cursor.execute(loc_query)
-    try:
-        (long_lat, city, state) = cursor.fetchone()
-    except TypeError as e:
-        print(e)
-        print("Couldn't process ", patent_id)
-        continue
+    (long_lat, city, state) = cursor.fetchone()
 
     # Patent may have several classes, for now info for the first primary class is retrieved
     class_query = ("SELECT current.patent_id, current.section_id, cpc_subsection.title as sub_sec, "
@@ -68,20 +63,29 @@ for inventor in inventor_ids:
         (pat_id, cpc_section_id, cpc_subsec, cpc_group, cpc_subgroup) = cursor.fetchone()
         cpc_section = patent_sections[cpc_section_id]
     except TypeError as e:
-        print(e)
-        print("Couldn't process ", patent_id)
+        print("Class query returned empty set for Patent ----> ", patent_id)
         continue
 
+    # Organization query might result multiple entries, for now only the first organization is used.
     organization_query = "SELECT organization FROM rawassignee where patent_id = '{0}'".format(patent_id)
     cursor.execute(organization_query)
-    try:
+    if cursor.rowcount == 1:
         organization = cursor.fetchone()
-    except TypeError as e:
-        print(e)
-        print("Couldn't process ", patent_id)
-        continue
+    else:
+        organization = [org[0] for org in cursor.fetchall()]
+    try:
+        organization[0]
+    except IndexError:
+        print(organization)
+        organization = ['']
 
-    writer. writerow([patent_id, inventor_id, name_first, name_last, cpc_section, cpc_subsec,
-                      cpc_group, cpc_subgroup, city, state, long_lat, organization[0]])
+    try:
+        inventor_features = [patent_id, title[0], inventor_id, name_first, name_last, cpc_section, cpc_subsec,
+                             cpc_group, cpc_subgroup, city, state, long_lat, organization[0]]
+        writer.writerow(inventor_features)
+    except UnicodeEncodeError as e:
+        print("Feature list generator failed for Patent ----> ", patent_id)
+        print(e)
+        continue
 
     print("CSV record for inventor:", inventor, " inserted")
