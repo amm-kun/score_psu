7297f9f8db1f93f2c936ec50d19e9cb69686b5df
Sree Sai Teja Lanka <lankasreesaiteja@gmail.com>
2020-08-27 21:18:58 -0400

    Update p_value.py

--- pipeline/p_value.py
+++ pipeline/p_value.py
@@ -22,7 +22,7 @@ def get_p_val_darpa_tsv(claim):
     
     # [p|P]\\s?[<>=]\\s?\\d?\\.\\d+e?[-|–]?\\d*
     
-    pattern_p = re.search("[p|P]\s*[<>=]\s*\d*\.\d+(e[-–]\d*)?(?!.*(-|–))", claim)
+    pattern_p = re.search("[p|P]\s*[<>=]\s*\d*\.\d+(e[-–]\d*)?(?!.*(-|–))(?!.*e)", claim)
     if pattern_p:
         return pattern_p.group()
     else:
